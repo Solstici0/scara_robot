ff8569e5083cc3cb78c3ce728eb1bb0cf2a75782
Ismael Jaras <is.jaras@gmail.com>
2023-04-30 00:48:48 -0400

    improve setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 # encoding: utf8
 from setuptools import setup
 
+def read_requirements():
+    requirements = []
+    with open("./requirements.txt") as f:
+        for req in f:
+            req = req.replace("\n", " ")
+            requirements.append(req)
+    return requirements
+
+
+def read_description():
+    with open("README.md", "r") as f:
+        descr = f.read()
+    return descr
+
 setup(
     name='scara',
     version='0.0.1',
     description='Solsticios Selective Compliance Articulated Robot Arm package',
+    long_description=read_description(),
+    long_description_content_type="text/md",
     packages=setuptools.find_packages(),
     author='Solsticio',
     author_email='contacto@solstic.io',
     url='https://github.com/Solstici0/scara',
+    license_files=('LICENSE.txt',),
 
 )
