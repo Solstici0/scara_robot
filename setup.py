79da79e3b185326ad6e4a0ac37b18340d5229329
Ismael Jaras <is.jaras@gmail.com>
2023-05-01 20:47:00 -0400

    update version and include classifiers

--- setup.py
+++ setup.py
@@ -17,15 +17,19 @@ def read_description():
 
 setup(
     name='scara',
-    version='0.0.2',
+    version='0.0.3',
     description='Solsticios Selective Compliance Articulated Robot Arm package',
     long_description=read_description(),
     long_description_content_type="text/markdown",
     packages=["scara"],
     install_requires=read_requirements(),
-    author='Solsticio',
-    author_email='contacto@solstic.io',
-    url='https://github.com/Solstici0/scara',
-    license_files=('LICENSE',),
-
+    author="Solsticio",
+    author_email="contacto@solstic.io",
+    url="https://github.com/Solstici0/scara",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineeringi"
 )
