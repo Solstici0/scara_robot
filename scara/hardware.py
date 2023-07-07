5a25a0371cbc6480775145f9afa0c69ffd2bed8a
Ismael Jaras <is.jaras@gmail.com>
2023-07-07 19:41:37 -0400

    include hardware abstraction layer

---
+++ scara/hardware.py
@@ -0,0 +1,20 @@
+"""
+hardware.py:
+    hardware base class and implementations
+"""
+
+class Hardware():
+    """
+    Hardware class
+    """
+    def __init__(self):
+        pass
+
+    def setup_routine(self):
+        pass
+
+    def move2_routine(self, position_inc_corrected, from_goal_point):
+        pass
+
+    def get_errors_routine(self):
+        pass
