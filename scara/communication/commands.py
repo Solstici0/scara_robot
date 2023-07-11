7bfebf89424758ab561ec37a20253848936b6b81
Ismael Jaras <is.jaras@gmail.com>
2023-07-10 21:50:40 -0400

    include protocol commands

---
+++ scara/communication/commands.py
@@ -0,0 +1,13 @@
+"""
+commands for interacting with scara robot through tcp/ip protocol
+"""
+## common
+HEADERSIZE = 100
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "EXIT"
+
+## server
+SERVER_WELCOME = "Hey there! you are communicating with the scara's raspy. " + \
+                "Remeber: \n Scara is caring <3"
+
+## client
