1b07fc7fd0d1d66c444f32c9e4ac3ec4808661af
Felipe Guajardo <fgerman.mena@gmail.com>
2023-09-14 12:51:58 -0300

    new kinematic functions and its respective test

--- scara/tools/kinematics.py
+++ scara/tools/kinematics.py
@@ -15,24 +15,58 @@ joints, dimensions = mng.load_robot_config('scara/config/default.yaml')
 
 
 red_ratio=7
-r1=1
-r2=0.5
-maxtheta1=[0,math.pi]
-martheta2=[math.pi/4,7*math.pi/4]
+r1=dimensions["humero_len"]
+r2=dimensions["radio_cubito_len"]
+maxtheta1=[0, 2*dimensions["max_hombro_degree"]]
+maxtheta2=[-1*dimensions["max_codo_degree"],dimensions["max_codo_degree"]]
+maxr = (r1+r2)**2
 
-def directKin(theta1,theta2):
+tolerance = 1e-12
+def direct_kin(theta1,theta2):
     theX=r1*math.cos(theta1)+r2*math.cos(theta2+theta1)
     theY=r1*math.sin(theta1)+r2*math.sin(theta2+theta1)
     return [theX,theY]
 
+def check_angle(angle,limits):
+    if angle < limits[0]:
+        if limits[0]-angle < tolerance:
+            return limits[0]
+        new_angle = angle + 2*math.pi
+        if limits[0] <= new_angle <= limits[1]:
+            return new_angle
+        elif limits[1] - new_angle <= tolerance:
+            return limits[1]
+        else:
+            #in this case the point is not achievable
+            return None
+    elif angle > limits[1]:
+        if angle-limits[1] < tolerance:
+            return limits[1]
+        new_angle = angle - 2*math.pi
+        if limits[0] <= new_angle <= limits[1]:
+            return new_angle
+        else:
+            #in this case the point is not achievable
+            return None
+    else:
+        return angle
 
-def inverseKin(x,y,ori="der"):
+def inverse_kin(x,y,ori="right"):
     rcuadrado=x**2+y**2
+    if rcuadrado == maxr:
+        return {"hombro":math.atan2(y,x),
+                "codo":0}
+    
     B=(rcuadrado-r1**2-r2**2)/(2*r1*r2)
     theta2=math.atan2(math.sqrt(1-B*B),B)
-    if (ori=="der"):
+    if (ori=="right"):
         theta1=math.atan2(y,x)-math.atan2(r2*math.sin(theta2),(r1+r2*math.cos(theta2)))
-    elif (ori=="izq"):
+    elif (ori=="left"):
         theta2=2*math.pi-theta2
         theta1=math.atan2(y,x)-math.atan2(r2*math.sin(theta2),(r1+r2*math.cos(theta2)))
-    return [theta1,theta2]
+    
+    theta1 = check_angle(theta1,maxtheta1)
+    theta2 = check_angle(theta2,maxtheta2)
+    return {"hombro":theta1,
+            "codo":theta2}
+
--- test/test_kinematics.py
+++ test/test_kinematics.py
@@ -1,5 +1,77 @@
+import math
 import pytest
 import scara.tools.kinematics as kin
 
 def test_correct_import():
-    assert  330.15  == kin.dimensions['humero_len']
+    assert  330.15  == kin.r1
+    assert 336.0 == kin.r2
+
+def test_estiradito_is_0_in_x():
+    ik_dict = kin.inverse_kin(0,330.15+336.0)
+    assert ik_dict["hombro"] == math.pi/2
+    assert ik_dict["codo"] == 0
+
+def test_max_x():
+    ik_dict = kin.inverse_kin(330.15+336.0,0)
+    assert ik_dict["hombro"] == 0
+    assert ik_dict["codo"] == 0
+
+def test_general_case():
+    expected_hombro  = math.pi/4
+    expected_codo = math.pi/4
+    given_x, given_y = kin.direct_kin(expected_hombro,expected_codo)
+    ik_dict = kin.inverse_kin(given_x,given_y)
+    assert expected_hombro == pytest.approx(ik_dict["hombro"])
+    assert expected_codo == pytest.approx(ik_dict["codo"])
+
+
+def test_general_case_left():
+    expected_hombro  = math.pi/4
+    expected_codo = -1*math.pi/4
+    given_x, given_y = kin.direct_kin(expected_hombro,expected_codo)
+    ik_dict = kin.inverse_kin(given_x,given_y,'left')
+    assert expected_hombro == pytest.approx(ik_dict["hombro"])
+    assert expected_codo == pytest.approx(ik_dict["codo"])
+
+def print_report_ik(x,y,expected_hombro,expected_codo,ik_dict):
+    format_string = 'x, y = %2f, %2f \n'\
+                                +'          expected , actual\n'\
+                                +'hombro | %2f, %2f\n'\
+                                +'codo   | %2f, %2f\n'
+    if ik_dict['hombro']:
+        actual_hombro  = ik_dict['hombro']
+    else:
+        actual_hombro = 666.0
+    if ik_dict['codo']:
+        actual_codo = ik_dict['codo']
+    else:
+        actual_codo = 666.0
+    result_string = format_string % (x,y,
+                                    expected_hombro,actual_hombro,
+                                    expected_codo,actual_codo)
+    print(result_string)
+
+def test_a_lot_of_cases():
+    number_of_points = 1000
+    theta1_step_size = (kin.maxtheta1[1]-kin.maxtheta1[0])/(number_of_points-1)
+    theta2_step_size = (kin.maxtheta2[1]-kin.maxtheta2[0])/(number_of_points-1)
+    theta1_list = [kin.maxtheta1[0] + i*theta1_step_size for i in range(number_of_points)]
+    theta2_list = [kin.maxtheta2[0] + i*theta2_step_size for i in range(number_of_points)]
+    for expected_hombro in theta1_list:
+        for expected_codo in theta2_list:
+            x,y = kin.direct_kin(expected_hombro,expected_codo)
+            if expected_codo<0:
+                ik_dict = kin.inverse_kin(x,y,'left')
+            else:
+                ik_dict = kin.inverse_kin(x,y,'right')
+            if expected_hombro == pytest.approx(ik_dict['hombro']):
+                assert True
+            else:
+                print_report_ik(x,y,expected_hombro,expected_codo,ik_dict)
+                assert False
+            if expected_codo == pytest.approx(ik_dict['codo']):
+                assert True
+            else:
+                print_report_ik(x,y,expected_hombro,expected_codo,ik_dict)
+                assert False           
+
