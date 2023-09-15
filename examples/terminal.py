e387554e3f652b6797a4f3a38404514fdc9f0554
Felipe Guajardo <fgerman.mena@gmail.com>
2023-09-15 11:58:29 -0300

    created an interactive ipython script

---
+++ examples/terminal.py
@@ -0,0 +1,15 @@
+import IPython
+from IPython.terminal.embed import InteractiveShellEmbed
+import scara
+import logging 
+def main():
+    scara.logger.setLevel(logging.INFO)  # set info level for logger
+    nelen = scara.Robot()  # creates an instance of the scara robot
+
+     # Start an IPython terminal
+    banner = "Welcome to the scara IPython terminal"
+    ipshell = InteractiveShellEmbed(banner1=banner)
+    ipshell()
+
+if __name__ == "__main__":
+    main()
