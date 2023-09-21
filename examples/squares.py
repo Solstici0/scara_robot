9e49fca2a6d9657fda782b7aa1a24bf4f3584c0c
Felipe Guajardo <fgerman.mena@gmail.com>
2023-09-20 21:21:25 -0300

    changed the setpoints

--- examples/squares.py
+++ examples/squares.py
@@ -14,10 +14,10 @@ nelen = scara.Robot()
 n_squares = 5
 # vertex defines each vertex of the square
 # vertex = [x_position, y_position, z_position]
-vertex_1 = [-50, 400, 100]
-vertex_2 = [-50, 500, 50]
-vertex_3 = [50, 500, 150]
-vertex_4 = [50, 400, 50]
+vertex_1 = [-100, 400, 100]
+vertex_2 = [-100, 500, 50]
+vertex_3 = [100, 500, 150]
+vertex_4 = [100, 400, 50]
 all_vertex = [vertex_1, vertex_2, vertex_3, vertex_4]
 def square_movement():
     for square_num in range(n_squares):
