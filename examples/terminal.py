0fde5f9296845e9b65b99e35ddbd86a317d589b5
Felipe Guajardo <fgerman.mena@gmail.com>
2023-09-24 12:26:22 -0300

    added pizza to terminal

--- examples/terminal.py
+++ examples/terminal.py
@@ -2,6 +2,8 @@ import IPython
 from IPython.terminal.embed import InteractiveShellEmbed
 import scara
 import logging 
+import scara.can_pizza as pizza
+
 def main():
     scara.logger.setLevel(logging.INFO)  # set info level for logger
     nelen = scara.Robot()  # creates an instance of the scara robot
